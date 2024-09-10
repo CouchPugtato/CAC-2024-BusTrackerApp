b6d29e35ad6fa7d03d9a9c940a3d274d149ab9cb
MrGrian <nehemiahchao@gmail.com>
2024-09-10 17:07:48 -0400

    blah

--- BusTrackerApp/app/(tabs)/index.tsx
+++ BusTrackerApp/app/(tabs)/index.tsx
@@ -1,9 +1,11 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, View } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import Mapview from 'react-native-maps';
+import React from 'react';
 
 export default function HomeScreen() {
   return (
