d1ca652b864f4bff9e527c2af43085ce4b933161
MrGrian <nehemiahchao@gmail.com>
2024-09-15 23:11:18 -0400

    marker on map works and working on sign in page

--- BusTrackerApp/app/(tabs)/maps.tsx
+++ BusTrackerApp/app/(tabs)/maps.tsx
@@ -1,11 +1,65 @@
 import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import MapView, {Marker } from 'react-native-maps';
+import { StyleSheet, View, Platform, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+
+        let locationData = await Location.getCurrentPositionAsync({});
+        setLocation(locationData);
+      } catch (error) {
+        setErrorMsg('Error getting location');
+      }
+    };
+
+    fetchLocation();
+  }, []);
+
+  let latitudeVal;
+  let longitudeVal;
+  let text;
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    const { coords } = location;
+    const { latitude, longitude } = coords;
+    text= `Latitude: ${latitude}\nLongitude: ${longitude}`;
+    longitudeVal =  longitude;
+    latitudeVal = latitude;
+  
+
+  } else {
+    text = 'Waiting...';
+  }
+
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
+    <View style={styles.container}>   
+      <MapView style={styles.map}
+        initialRegion = {{
+          latitude: latitudeVal,
+          longitude:  longitudeVal,
+          latitudeDelta: 0.0,
+          longitudeDelta: 0.0,
+        }} >
+        <Marker coordinate = {{latitude: latitudeVal,longitude: longitudeVal}}
+         pinColor = {"purple"} // any color
+         title={"My Location"}
+         description={" blah"}/>
+      </MapView>
     </View>
   );
 }
